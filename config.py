6973f08565899e207c1217a5e0b0afa324a82f5b
Kacper Szmigiel <kacper.szmigiel@makimo.pl>
2017-09-12 20:10:04 +0200

    changes to work with python 3

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import ConfigParser
+import configparser
 import threading
 import paramiko
 import os
@@ -6,30 +6,30 @@ import sys
 import time
 
 if len(sys.argv) < 2:
-    print "Not enough parameters"
+    print('Not enough parameters')
     exit(1)
 
 f_path = sys.argv[1]
 if not os.path.exists(sys.argv[1]):
-    print "File {f_path} does not exist".format(f_path=f_path)
+    print('File {f_path} does not exist'.format(f_path=f_path))
     exit(2)
 
-config = ConfigParser.RawConfigParser()
+config = configparser.RawConfigParser()
 fp = open(f_path)
 config.readfp(fp)
 
 sections = config.sections()
-if "commands" not in sections:
-    print "You need to have a `commands` section in your config file"
+if 'commands' not in sections:
+    print('You need to have a `commands` section in your config file')
     exit(3)
 
 servers = list(sections)
 servers.remove('commands')
 commands = config.items('commands')
 
-str_format = "{alias:<10} {command:<10} {output:>90}"
-print str_format.format(alias="Alias", command="Command", output="Output (stdout and stderr)")
-print str_format.format(alias="-"*5, command="-"*7, output="-"*90)
+str_format = '{alias:<10} {command:<10} {output:>90}'
+print(str_format.format(alias='Alias', command='Command', output='Output (stdout and stderr)'))
+print(str_format.format(alias='-'*5, command='-'*7, output='-'*90))
 
 
 def run_cmd(srv, hostname, username, password, cmd, cmd_id):
@@ -40,7 +40,7 @@ def run_cmd(srv, hostname, username, password, cmd, cmd_id):
                    password=password)
     stdin, stdout, stderr = client.exec_command(cmd)
 
-    output = stdout.read().strip()+" "+stderr.read().strip()
+    output = '{} {}'.format(stdout.read().strip().decode('utf-8'), stderr.read().strip().decode('utf-8'))
 
     sys.stdout.write(str_format.format(alias=srv,
                             command=cmd_id,
@@ -68,5 +68,4 @@ for command in commands:
 while True in [t.isAlive() for t in threads]:
     time.sleep(0.2)
 
-print ""
-print "Threads used: {thread_count}".format(thread_count=thread_count)
+print('Threads used: {thread_count}'.format(thread_count=thread_count))
